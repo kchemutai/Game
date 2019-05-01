7b9dd72eea0ce239867fb0a82ee699c5009664ed
kchemutai <kchemutai@gmail.com>
2019-05-01 17:55:12 +0300

    Working code for Rock Scissors paper

--- js/index.js
+++ js/index.js
@@ -1,44 +1,84 @@
-var playerSelection=window.prompt("Choose Rock, Paper or Scissors");
-playerSelection=playerSelection.toLocaleLowerCase();
-var computerWin;
-var playerWin;
+var computerWin=0;
+var playerWin=0;
 function playRound(playerSelection, computerSelection) {
- 
     if(playerSelection===computerSelection){
-      return ("Its a Tie  "+"Your Selection: "+playerSelection+" computerSelection: "+computerSelection);
+      return ("Its a Tie");
     }
-    else if(playerSelection==="scissors" && computerSelection==="paper"){
-          
-          return ("You Win "+"Your Selection: "+playerSelection+" computerSelection: "+computerSelection);
-          playerWin++;
-          
-    }
-    else if(playerSelection==="rock" && computerSelection==="paper"){
+    
+    //scissors
+    else if(playerSelection==="scissors"){
+
+          if(computerSelection==="rock"){
             
-            return ("Computer Wins "+"Your Selection: "+playerSelection+" computerSelection: "+computerSelection);
             computerWin++;
+            return ("Computer wins");
+          }
+          else{
+          
+          playerWin++;
+          return ("You Win");
+          }
     }
+    
+  //rock
+    else if(playerSelection==="rock"){
+
+      if(computerSelection==="paper"){
+        
+        computerWin++;
+        return ("Computer win");
+      }
+      else{
+      
+      playerWin++;
+      return ("You Win");
+      }
+    }
+
+    //paper
+   else if(playerSelection==="paper"){
+
+      if(computerSelection==="scissors"){
+       
+        computerWin++;
+        return ("Computer wins");
+      }
+      else{
+      playerWin++;
+      return ("You Win");
+      }
+    }
+
+    
   }
 
-function computerPlay(){
-  const options=['rock','paper','scissors'];
-  var x=Math.floor(Math.random() * 3);
-  return options[x];
-}
-var computerSelection=computerPlay();
+  function computerPlay(){
+    const options=['rock','paper','scissors'];
+    var x=Math.floor(Math.random() * 3);
+    return options[x];
+  }
+  var computerSelection=computerPlay();
+   
+
 
 function game(){
-    for(var i=0;i<5;i++){
-      playRound(playerSelection, computerSelection);
-    }
-    if(playerWin>computerWin){
-        return ("Player wins by ",+playerWin);
-    }
-    else{
-        return ("computer wins by ",+computerWin);
-    }
+          for (var i = 0; i < 5; i++)
+          {
+          var playerSelection=window.prompt("Choose Rock, Paper or Scissors");
+          playerSelection=playerSelection.toLocaleLowerCase();
+          console.log(playRound(playerSelection, computerSelection));
+            
+          }
+       ///determine winner
+        if(playerWin>computerWin){
+          return ("Player Wins Overall "+playerWin);
+        }
+        else if(playerWin<computerWin){
+          return ("Computer Wins Overall "+computerWin);
+        }
+        else{
+          return "Its an equal score ";
+        }
+  }
+  console.log(game());
 
-}
-
-
-console.log(game());
